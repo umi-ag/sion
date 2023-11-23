66c320fd77be8a99bd4da3d6c8ce28a87236ed15
imataka7 <t.imagawa@kakehashi.life>
2023-11-24 01:22:26 +0900

    Refactor

--- gdrive-ui/src/components/FooterMenu/index.tsx
+++ gdrive-ui/src/components/FooterMenu/index.tsx
@@ -1,35 +1,10 @@
-import clsx from 'clsx';
-import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import React from 'react';
 import { IoCarSportOutline } from 'react-icons/io5';
 import { LuMailPlus } from 'react-icons/lu';
 import { PiCertificate } from 'react-icons/pi';
 import { SlPresent } from 'react-icons/sl';
-
-const MenuItem: React.FC<{
-  icon: React.ReactNode;
-  label: string;
-  href: string;
-  isActive?: boolean;
-}> = ({ icon, label, href, isActive }) => {
-  const containerClass = clsx(
-    'flex flex-col gap-1 justify-center items-center relative',
-    // isActive && '!text-accent-700',
-  );
-
-  return (
-    <Link href={href}>
-      <div className={containerClass}>
-        <span className="text-4xl h-8">{icon}</span>
-        <span className="text-sm text-gray-500">{label}</span>
-        {isActive && (
-          <div className="absolute -bottom-1 left-0 w-full h-1 bg-gray-700 rounded-full" />
-        )}
-      </div>
-    </Link>
-  );
-};
+import { MenuItem } from './MenuItem';
 
 export const FooterMenu: React.FC = () => {
   const items = [
