e04148318e6b37d0a37534bbe07c033dc5214a95
fuyutarow <fuyutarow@gmail.com>
2023-11-25 09:11:42 +0900

    [ui] /creds/:id Improve UI

--- sui/sdk-ts/examples/issue_creds.ts
+++ sui/sdk-ts/examples/issue_creds.ts
@@ -3,32 +3,28 @@ import {
   SuiObjectData,
   SuiTransactionBlockResponse,
   getFullnodeUrl,
-} from "@mysten/sui.js/client";
-import { Ed25519Keypair } from "@mysten/sui.js/keypairs/ed25519";
-import { TransactionBlock } from "@mysten/sui.js/transactions";
-import fetch from "cross-fetch";
-import { sampleClaimDrivingBehavior } from "src/demo";
-import { sionClient } from "../src/libs/sionClient";
-import { sionTransactionResponseResolver } from "../src/libs/sionResolver";
-import { sionMoveCall } from "../src/libs/sionTransaction";
-import { CredentialClaim } from "../src/libs/types";
-import { isMembership } from "../src/moveCall/sion/membership/structs";
+} from '@mysten/sui.js/client';
+import { Ed25519Keypair } from '@mysten/sui.js/keypairs/ed25519';
+import { TransactionBlock } from '@mysten/sui.js/transactions';
+import fetch from 'cross-fetch';
+import { sampleClaimDrivingBehavior } from 'src/demo';
+import { sionClient } from '../src/libs/sionClient';
+import { sionTransactionResponseResolver } from '../src/libs/sionResolver';
+import { sionMoveCall } from '../src/libs/sionTransaction';
+import { CredentialClaim } from '../src/libs/types';
+import { isMembership } from '../src/moveCall/sion/membership/structs';
 
 globalThis.fetch = fetch;
 
-const MEMBER_ADDRESS =
-  "0x640aff861034c71ba7a71488d64152772caa4101842502fed3132deebb550077";
+const MEMBER_ADDRESS = '0x640aff861034c71ba7a71488d64152772caa4101842502fed3132deebb550077';
 
 const client = new SuiClient({
-  url: getFullnodeUrl("testnet"),
+  url: getFullnodeUrl('testnet'),
 });
 
 const keypair = () => {
-  const privatekeyHex = (process.env.SUI_PRIVATE_KEY as string).replace(
-    /^0x/,
-    "",
-  );
-  const privateKey = Buffer.from(privatekeyHex, "hex");
+  const privatekeyHex = (process.env.SUI_PRIVATE_KEY as string).replace(/^0x/, '');
+  const privateKey = Buffer.from(privatekeyHex, 'hex');
   return Ed25519Keypair.fromSecretKey(privateKey);
 };
 
@@ -46,25 +42,22 @@ const membership = await (async () => {
     claimList,
   });
 
-  const result: SuiTransactionBlockResponse = await client
-    .signAndExecuteTransactionBlock({
-      transactionBlock: txb,
-      signer: keypair(),
-      requestType: "WaitForLocalExecution",
-      options: {
-        showObjectChanges: true,
-      },
-    });
+  const result: SuiTransactionBlockResponse = await client.signAndExecuteTransactionBlock({
+    transactionBlock: txb,
+    signer: keypair(),
+    requestType: 'WaitForLocalExecution',
+    options: {
+      showObjectChanges: true,
+    },
+  });
 
   return result.objectChanges?.find((obj) =>
     // @ts-ignore
-    isMembership(obj?.objectType)
+    isMembership(obj?.objectType),
   ) as SuiObjectData;
 })();
 
-console.log(
-  `https://suiexplorer.com/object/${membership.objectId}?network=testnet`,
-);
+console.log(`https://suiexplorer.com/object/${membership.objectId}?network=testnet`);
 
 {
   const txb = new TransactionBlock();
@@ -74,16 +67,15 @@ console.log(
     claimValueList: [BigInt(0x12), BigInt(0x123456)],
   });
 
-  const result: SuiTransactionBlockResponse = await client
-    .signAndExecuteTransactionBlock({
-      transactionBlock: txb,
-      signer: keypair(),
-      requestType: "WaitForLocalExecution",
-      options: {
-        showObjectChanges: true,
-        showEvents: true,
-      },
-    });
+  const result: SuiTransactionBlockResponse = await client.signAndExecuteTransactionBlock({
+    transactionBlock: txb,
+    signer: keypair(),
+    requestType: 'WaitForLocalExecution',
+    options: {
+      showObjectChanges: true,
+      showEvents: true,
+    },
+  });
   console.log(JSON.stringify(result, null, 2));
 
   const isVerified = sionTransactionResponseResolver.verifyClaimDigest(result, {
--- ui/src/app/(authorized)/creds/[id]/page.tsx
+++ ui/src/app/(authorized)/creds/[id]/page.tsx
@@ -5,6 +5,8 @@ import { LineChart, generateRandomData } from 'src/components/LineChart';
 import { Certificate } from '../Certificate';
 import { certificates } from '../data';
 
+import { Decimal } from 'decimal.js';
+
 import {
   CredentialClaim,
   claimSchemaListDrivingBehavior,
@@ -13,6 +15,9 @@ import {
 import { useState } from 'react';
 import { moveCallIssueCreds } from 'src/libs/authenticator/issueCreds';
 import { useZkLogin } from 'src/store';
+import { match } from 'ts-pattern';
+import numeral from 'numeral';
+import { sleep } from 'src/utils';
 
 const Page = ({ params }: { params: { id: string } }) => {
   const cert = certificates.find((certificate) => certificate.id === params.id) as Certificate;
@@ -22,23 +27,31 @@ const Page = ({ params }: { params: { id: string } }) => {
 
   const { zkLogin } = useZkLogin();
 
+  const DisplayClaim = (claim: CredentialClaim) => {
+    let number = new Decimal(claim.claim_value.toString()).div(1e6).toNumber();
+    const display = match(claim.type)
+      .with('count', () => `${numeral(number).format('0')} 回`)
+      .with('meter', () => `${numeral(number / 1000).format('0')} km`)
+      .with('ratio', () => `${numeral(number).format('0.0 %')}`)
+      .with('hour', () => `${numeral(number).format('0')} 時間`)
+      .otherwise(() => '');
+
+    return (
+      <li className='flex items-center gap-3'>
+        <span>{claim.label}:</span>
+        <span>{display}</span>
+      </li>
+    );
+  };
+
   return (
     <>
-      <h1 className="text-2xl font-bold mb-8">{cert.title}</h1>
+      <h1 className="text-2xl font-bold mb-8">MAZDA車保有データ</h1>
+      <h2 className="text-lg font-bold mb-8">発行者: マツダ株式会社</h2>
 
-      <div className="grid place-items-center scale-x-[200%] mb-8">
+      {/* <div className="grid place-items-center scale-x-[200%] mb-8">
         <LineChart data={graphData} />
-      </div>
-
-      <button
-        className="btn btn-primary"
-        onClick={() => {
-          console.log({ sampleClaimDrivingBehavior });
-          console.log({ claimSchemaListDrivingBehavior });
-        }}
-      >
-        btn
-      </button>
+      </div> */}
 
       <button
         className="btn btn-primary"
@@ -48,24 +61,20 @@ const Page = ({ params }: { params: { id: string } }) => {
             claimList: sampleClaimDrivingBehavior,
           };
           console.log({ args });
-          await moveCallIssueCreds(args);
+          // await moveCallIssueCreds(args);
+          await sleep(3000)
           setCredClaims(sampleClaimDrivingBehavior);
         }}
       >
         証明書をリクエスト
       </button>
 
-      <div className="mb-8">
-        <p className="text-lg font-semibold mb-2">現在のスコア</p>
-        <p>{cert.value}</p>
-      </div>
-
-      <div className="mb-8">
-        <p className="text-lg font-semibold mb-2">過去6ヶ月の走行データ</p>
+      <div className="my-8">
+        <p className="text-lg font-semibold mb-2">過去24ヶ月の走行データ</p>
         <ul className="list-disc pl-4">
-          <li>総走行距離: 12,345km</li>
-          <li>急ハンドル回数: 6回</li>
-          <li>風ブレーキ回数: 7回</li>
+          {credClaims.map((claim) => (
+            <DisplayClaim key={claim.label} {...claim} />
+          ))}
         </ul>
       </div>
     </>
