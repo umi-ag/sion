f6c5f76d769ffe493b1217924d6af53d5c8ab946
imataka7 <t.imagawa@kakehashi.life>
2023-11-25 10:26:57 +0900

    Fix typos

--- ui/src/app/(authorized)/creds/page.tsx
+++ ui/src/app/(authorized)/creds/page.tsx
@@ -2,16 +2,16 @@
 
 import Link from 'next/link';
 import React from 'react';
-import { CredencialCard } from './CredentailCard';
-import { CredentailProfile, credencialList } from './data';
+import { CredentialCard } from './CredentialCard';
+import { credentialList } from './data';
 
 const Page = () => {
   return (
     <>
       <h1 className="text-2xl font-bold mb-8">証明書一覧</h1>
-      {credencialList.map((cred) => (
+      {credentialList.map((cred) => (
         <Link href={`/creds/${cred.id}`} key={cred.id}>
-          <CredencialCard cred={cred} />
+          <CredentialCard cred={cred} />
         </Link>
       ))}
     </>
