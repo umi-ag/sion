8ff5237e108e36475713f4fb8e1e0b7a9b532d0c
imataka7 <t.imagawa@kakehashi.life>
2023-12-03 01:09:56 +0900

    make possible to uploadFile

--- ui/src/app/(authorized)/gdrive/page.tsx
+++ ui/src/app/(authorized)/gdrive/page.tsx
@@ -1,40 +1,58 @@
 'use client';
 
 import Link from 'next/link';
-import { redirect } from 'next/navigation';
-import { useEffect } from 'react';
-import { useGdrive, useOauth } from 'src/store';
+import { useState } from 'react';
+import { useGdrive } from 'src/store';
 
 const Page = () => {
-  const { oauth } = useOauth();
-  const { useFiles } = useGdrive();
-
-  useEffect(() => {
-    if (oauth.loginStatus === 'loggedOut') {
-      redirect('/login');
-    }
-  }, [oauth]);
+  const { useFiles, uploadFile, uploadFile2 } = useGdrive();
+  const { files, isLoading } = useFiles();
+  const [file, setFile] = useState({
+    name: '',
+    content: '',
+  });
 
   return (
-    <div>
+    <div className="h-full overflow-y-scroll">
       <h1>gdrive</h1>
-      <p>
+      <p className="mb-8">
         <Link href="/login" className="text-blue-400 underline">
           Login
         </Link>
       </p>
 
-      <div>
-        <button className="border border-black px-4 py-1 rounded" onClick={() => {}}>
-          List
+      <div className="mb-8">
+        <input
+          type="text"
+          placeholder="file name"
+          className="input input-bordered input-primary input-sm w-full max-w-xs mb-4"
+          onInput={(e) => setFile({ ...file, name: e.currentTarget.value })}
+        />
+
+        <textarea
+          className="textarea textarea-bordered w-full mb-4"
+          placeholder="Bio"
+          onInput={(e) => setFile({ ...file, content: e.currentTarget.value })}
+        />
+
+        <button className="btn btn-accent" onClick={() => uploadFile(file as never)}>
+          Upload
+        </button>
+
+        <button className="btn btn-accent" onClick={() => uploadFile2()}>
+          Upload2
         </button>
       </div>
-      {useFiles.isLoading && <p>Loading...</p>}
-      <ul>
-        {useFiles.files.map((file) => (
-          <li key={file.id}>{file.name}</li>
-        ))}
-      </ul>
+
+      <details>
+        <summary>{isLoading ? <span>Loading...</span> : <span>Files</span>}</summary>
+
+        <ul>
+          {files.map((file) => (
+            <li key={file.id}>{file.name}</li>
+          ))}
+        </ul>
+      </details>
     </div>
   );
 };
--- ui/src/store/gdrive.ts
+++ ui/src/store/gdrive.ts
@@ -2,7 +2,7 @@
 
 import { File } from 'src/types';
 import useSWR from 'swr';
-import { useOauth } from './oauth';
+import { OauthState, useOauth } from './oauth';
 
 // export type GdriveState = {
 //   // gdrive
@@ -33,9 +33,74 @@ const listFiles = async (accessToken: string) => {
   return json.files as File[];
 };
 
-export const useFiles = () => {
-  const { oauth } = useOauth();
+const uploadFile = async (file: File, accessToken: string) => {
+  const url = `https://www.googleapis.com/upload/drive/v3/files?uploadType=media&key=${API_KEY}`;
 
+  const r = await fetch(url, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(file),
+  });
+
+  if (!r.ok) {
+    console.error(await r.json());
+    return;
+  }
+
+  const json = await r.json();
+  console.log(json);
+};
+
+const uploadFile2 = async (accessToken: string) => {
+  // バウンダリ文字列を定義
+  const boundary = '-------314159265358979323846';
+
+  // メタデータを定義
+  const metadata = {
+    name: 'ohayo', // ファイル名
+    mimeType: 'text/plain', // MIMEタイプ
+  };
+
+  // ファイルの中身
+  const content = 'おはよう';
+  const blob = new Blob([content], { type: 'text/plain' });
+
+  // リクエストボディを構築
+  let requestBody = '';
+  requestBody += `--${boundary}\r\n`;
+  requestBody += 'Content-Type: application/json; charset=UTF-8\r\n\r\n';
+  requestBody += `${JSON.stringify(metadata)}\r\n`;
+  requestBody += `--${boundary}\r\n`;
+  requestBody += `Content-Type: ${blob.type}\r\n\r\n`;
+  requestBody += `${content}\r\n`;
+  requestBody += `--${boundary}--`;
+
+  // fetch APIを使ってリクエストを送信
+  const r = await fetch('https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart', {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      'Content-Type': `multipart/related; boundary=${boundary}`,
+    },
+    body: requestBody,
+  });
+  // .then((response) => response.json())
+  // .then((data) => console.log(data))
+  // .catch((error) => console.error('Error:', error));
+
+  if (!r.ok) {
+    console.error(await r.json());
+    return;
+  }
+
+  const json = await r.json();
+  console.log(json);
+};
+
+export const useFiles = (oauth: OauthState) => {
   const shouldFetch = !!oauth.accessToken;
   const key = shouldFetch ? ['gdrive-list-files', oauth.accessToken] : null;
   const { data, ...rest } = useSWR(key, ([_, accessToken]) => listFiles(accessToken));
@@ -47,7 +112,11 @@ export const useFiles = () => {
 };
 
 export const useGdrive = () => {
+  const { oauth } = useOauth();
+
   return {
-    useFiles: useFiles(),
+    useFiles: () => useFiles(oauth),
+    uploadFile: (file: File) => uploadFile(file, oauth.accessToken),
+    uploadFile2: () => uploadFile2(oauth.accessToken),
   };
 };
