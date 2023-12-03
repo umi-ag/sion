e243c6e2059d1fc808f36126080c19b03f4543cf
imataka7 <t.imagawa@kakehashi.life>
2023-12-03 17:47:18 +0900

    refactor

--- ui/src/store/gdrive.ts
+++ ui/src/store/gdrive.ts
@@ -2,7 +2,7 @@
 
 import { File } from 'src/types';
 import useSWR from 'swr';
-import { OauthState, useOauth } from './oauth';
+import { useOauth } from './oauth';
 
 // export type GdriveState = {
 //   // gdrive
@@ -33,63 +33,44 @@ const listFiles = async (accessToken: string) => {
   return json.files as File[];
 };
 
-const uploadFile = async (file: File, accessToken: string) => {
-  const url = `https://www.googleapis.com/upload/drive/v3/files?uploadType=media&key=${API_KEY}`;
-
-  const r = await fetch(url, {
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(file),
-  });
-
-  if (!r.ok) {
-    console.error(await r.json());
-    return;
-  }
-
-  const json = await r.json();
-  console.log(json);
+export type NewFile = {
+  name: string;
+  mimeType: string;
+  content: string;
 };
 
-const uploadFile2 = async (accessToken: string) => {
-  // バウンダリ文字列を定義
-  const boundary = '-------314159265358979323846';
+// バウンダリ文字列を定義
+const BOUNDARY = '-------314159265358979323846';
 
+const uploadFile = async ({ name, mimeType, content }: NewFile, accessToken: string) => {
   // メタデータを定義
   const metadata = {
-    name: 'ohayo', // ファイル名
-    mimeType: 'text/plain', // MIMEタイプ
+    name, // ファイル名
+    mimeType, // MIMEタイプ
   };
 
   // ファイルの中身
-  const content = 'おはよう';
-  const blob = new Blob([content], { type: 'text/plain' });
+  const blob = new Blob([content], { type: mimeType });
 
   // リクエストボディを構築
   let requestBody = '';
-  requestBody += `--${boundary}\r\n`;
+  requestBody += `--${BOUNDARY}\r\n`;
   requestBody += 'Content-Type: application/json; charset=UTF-8\r\n\r\n';
   requestBody += `${JSON.stringify(metadata)}\r\n`;
-  requestBody += `--${boundary}\r\n`;
+  requestBody += `--${BOUNDARY}\r\n`;
   requestBody += `Content-Type: ${blob.type}\r\n\r\n`;
   requestBody += `${content}\r\n`;
-  requestBody += `--${boundary}--`;
+  requestBody += `--${BOUNDARY}--`;
 
   // fetch APIを使ってリクエストを送信
   const r = await fetch('https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart', {
     method: 'POST',
     headers: {
       Authorization: `Bearer ${accessToken}`,
-      'Content-Type': `multipart/related; boundary=${boundary}`,
+      'Content-Type': `multipart/related; boundary=${BOUNDARY}`,
     },
     body: requestBody,
   });
-  // .then((response) => response.json())
-  // .then((data) => console.log(data))
-  // .catch((error) => console.error('Error:', error));
 
   if (!r.ok) {
     console.error(await r.json());
@@ -98,11 +79,13 @@ const uploadFile2 = async (accessToken: string) => {
 
   const json = await r.json();
   console.log(json);
+
+  return json;
 };
 
-export const useFiles = (oauth: OauthState) => {
-  const shouldFetch = !!oauth.accessToken;
-  const key = shouldFetch ? ['gdrive-list-files', oauth.accessToken] : null;
+export const useFiles = (accessToken: string) => {
+  const shouldFetch = !!accessToken;
+  const key = shouldFetch ? ['gdrive-list-files', accessToken] : null;
   const { data, ...rest } = useSWR(key, ([_, accessToken]) => listFiles(accessToken));
 
   return {
@@ -115,8 +98,7 @@ export const useGdrive = () => {
   const { oauth } = useOauth();
 
   return {
-    useFiles: () => useFiles(oauth),
-    uploadFile: (file: File) => uploadFile(file, oauth.accessToken),
-    uploadFile2: () => uploadFile2(oauth.accessToken),
+    useFiles: () => useFiles(oauth.accessToken),
+    uploadFile: (file: NewFile) => uploadFile(file, oauth.accessToken),
   };
 };
